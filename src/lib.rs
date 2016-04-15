c12a2ee4f499780db7b0682c086973ddff8fbf69
Richard Protasov <richardprotasov@gmail.com>
2016-04-14 23:17:15 -0400

    👷  reformats

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,12 @@ use std::env;
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
+    let database_url = 
+        env::var("DATABASE_URL")
+            .expect("DATABASE_URL must be set");
+
     PgConnection::establish(&database_url)
-        .expect(&format!("Error connection to {}", database_url))
+        .expect(
+            &format!("Error connection to {}", database_url)
+        )
 } 
